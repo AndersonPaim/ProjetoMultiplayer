bde8d539e26025d15a23d96e074fa6565da7cef2
Anderson Paim <andersonpaim15@outlook.com>
2021-09-04 02:41:09 -0300

    Added start button

--- TheWildIsland/Assets/Mirror/Examples/TheWildIsland/Scripts/GameSystem.cs
+++ TheWildIsland/Assets/Mirror/Examples/TheWildIsland/Scripts/GameSystem.cs
@@ -6,11 +6,35 @@ namespace Mirror.Examples.Pong
     // spawning players. The built in RoundRobin spawn method wouldn't work after
     // someone reconnects (both players would be on the same side).
     [AddComponentMenu("")]
-    public class NetworkManagerPong : NetworkManager
+    public class GameSystem : NetworkManager
     {
         public Transform leftRacketSpawn;
         public Transform rightRacketSpawn;
         GameObject ball;
+
+        private bool _hasStarted = false;
+        
+        private void OnGUI()
+        {
+            Debug.Log(IsServer);
+            
+            if (!IsServer)
+            {
+                return;
+            }
+
+            Vector2 pos = new Vector2(100, Screen.height - 200);
+            Vector2 size = new Vector2(200, 100);
+
+            if (!_hasStarted)
+            {
+                if (GUI.Button(new Rect(pos, size), "Begin Match"))
+                {
+                    OnMatchBegin();
+                }
+            }
+        }
+
 
         public override void OnServerAddPlayer(NetworkConnection conn)
         {
@@ -18,13 +42,6 @@ namespace Mirror.Examples.Pong
             Transform start = numPlayers == 0 ? leftRacketSpawn : rightRacketSpawn;
             GameObject player = Instantiate(playerPrefab, start.position, start.rotation);
             NetworkServer.AddPlayerForConnection(conn, player);
-
-            // spawn ball if two players
-            if (numPlayers == 2)
-            {
-                ball = Instantiate(spawnPrefabs.Find(prefab => prefab.name == "Ball"));
-                NetworkServer.Spawn(ball);
-            }
         }
 
         public override void OnServerDisconnect(NetworkConnection conn)
@@ -35,6 +52,20 @@ namespace Mirror.Examples.Pong
 
             // call base functionality (actually destroys the player)
             base.OnServerDisconnect(conn);
+        }
+
+        private void OnMatchBegin()
+        {
+            if (numPlayers != 2)
+            {
+                Debug.LogError("Need two connected players!");
+                return;
+            }
+            
+            _hasStarted = true;
+            
+            ball = Instantiate(spawnPrefabs.Find(prefab => prefab.name == "Ball"));
+            NetworkServer.Spawn(ball);
         }
     }
 }
--- TheWildIsland/Assets/Mirror/Runtime/NetworkManager.cs
+++ TheWildIsland/Assets/Mirror/Runtime/NetworkManager.cs
@@ -14,8 +14,12 @@ namespace Mirror
     [DisallowMultipleComponent]
     [AddComponentMenu("Network/NetworkManager")]
     [HelpURL("https://mirror-networking.gitbook.io/docs/components/network-manager")]
-    public class NetworkManager : MonoBehaviour
+    public class NetworkManager : NetworkBehaviour
     {
+        public bool IsServer => isServer;
+        public bool IsServerOnly => isServerOnly;
+        public bool HasAuthority => hasAuthority;
+        
         /// <summary>Enable to keep NetworkManager alive when changing scenes.</summary>
         // This should be set if your game has a single NetworkManager that exists for the lifetime of the process. If there is a NetworkManager in each scene, then this should not be set.</para>
         [Header("Configuration")]
