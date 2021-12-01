4ced308f0b66a830bed16bfa7739fd22c735648a
Anderson Paim <andersonpaim15@outlook.com>
2021-11-30 22:26:22 -0300

    Added camera shake

---
+++ TheWildIsland/Assets/CameraShakeFX/Scripts/TraumaInducer.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+using System.Collections;
+using Mirror.Examples.Pong;
+
+
+namespace Mirror.Examples.Pong
+{
+    public class TraumaInducer : NetworkBehaviour
+    {
+        [Tooltip("Seconds to wait before trigerring the explosion particles and the trauma effect")]
+        public float Delay = 0;
+        [Tooltip("Maximum stress the effect can inflict upon objects Range([0,1])")]
+        public float MaximumStress = 3f;
+        [Tooltip("Maximum distance in which objects are affected by this TraumaInducer")]
+        public float Range = 45;
+
+        private GameObject _camera;
+
+        private void Start()
+        {
+            _camera = Camera.main.gameObject;
+            //ShakeCamera();
+        }
+
+        public void SetRange(float range)
+        {
+            Range = range;
+        }
+
+        public void Shake()
+        {
+            ShakeCamera();
+        }
+
+        private void ShakeCamera()
+        {
+            /* Find all gameobjects in the scene and loop through them until we find all the nearvy stress receivers */
+            var targets = UnityEngine.Object.FindObjectsOfType<GameObject>();
+            for(int i = 0; i < targets.Length; ++i)
+            {
+                var receiver = targets[i].GetComponent<Player>();
+
+                if(receiver == null) continue;
+                float distance = Vector3.Distance(transform.position, targets[i].transform.position);
+                /* Apply stress to the object, adjusted for the distance */
+                if(distance > Range) continue;
+                float distance01 = Mathf.Clamp01(distance / Range);
+                float stress = (1 - Mathf.Pow(distance01, 2)) * MaximumStress;
+                _camera.GetComponent<StressReceiver>().InduceStress(stress);
+            }
+        }
+    }
+}
--- TheWildIsland/Assets/_Project/Scripts/Game/Bullet.cs
+++ TheWildIsland/Assets/_Project/Scripts/Game/Bullet.cs
@@ -6,6 +6,7 @@ namespace Mirror.Examples.Pong
 {
     public class Bullet : NetworkBehaviour
     {
+        [SerializeField] private TraumaInducer _shake;
         public int tipoArma;
        // public WeaponBalancer[] _weaponBalancer;
         public WeaponBalancer _weaponBalancer;
@@ -14,13 +15,10 @@ namespace Mirror.Examples.Pong
 
         public void Shoot()
         {
-            //TODO PASSAR SCRIPTABLE OBJECTS COM OS STATS DA BULLET
             _rb = gameObject.GetComponent<Rigidbody2D>();
             _rb.velocity = transform.right * _weaponBalancer.velocity;
 
-
             _rb.mass = _weaponBalancer.weight;
-            Debug.Log("tipoArma_" + tipoArma);
         }
 
         public void DestroyBullet()
@@ -54,6 +52,12 @@ namespace Mirror.Examples.Pong
                     {
                         Vector2 direction = collider.transform.position - transform.position;
                         collider.GetComponent<Rigidbody2D>().AddForce(direction * _weaponBalancer.knockback);
+                        _shake.SetRange(_weaponBalancer.blastRadius * 2);
+
+                        if(player.hasAuthority)
+                        {
+                            _shake.Shake();
+                        }
                     }
                 }
 
